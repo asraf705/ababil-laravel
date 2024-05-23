0be1373a5a478b4a9c0ab6f418655a698098c416
Asrafuzzaman <asrfuzzaman2379@gmail.com>
2024-05-23 12:21:39 +0600

    update

--- app/Models/Policy.php
+++ app/Models/Policy.php
@@ -22,18 +22,33 @@ class Policy extends Model
         $privacyPolicy->four_name                = $request->four_name;
         $privacyPolicy->four_policy              = $request->four_policy;
         $privacyPolicy->save();
-        return $privacyPolicy;
     }
 
     public static function newPrivacyPolicy($request)
     {
         self::$privacyPolicy = new Policy();
-        self::saveBasicInfo(self::$privacyPolicy, $request);
+        self::$privacyPolicy->one_name                  = $request->one_name;
+        self::$privacyPolicy->one_policy                = $request->one_policy;
+        self::$privacyPolicy->two_name                  = $request->two_name;
+        self::$privacyPolicy->two_policy                = $request->two_policy;
+        self::$privacyPolicy->three_name                = $request->three_name;
+        self::$privacyPolicy->three_policy              = $request->three_policy;
+        self::$privacyPolicy->four_name                 = $request->four_name;
+        self::$privacyPolicy->four_policy               = $request->four_policy;
+        self::$privacyPolicy->save();
     }
 
     public static function updatePrivacyPolicy($request, $id)
     {
         self::$privacyPolicy = Product::find($id);
-        self::saveBasicInfo(self::$privacyPolicy, $request);
+        self::$privacyPolicy->one_name                  = $request->one_name;
+        self::$privacyPolicy->one_policy                = $request->one_policy;
+        self::$privacyPolicy->two_name                  = $request->two_name;
+        self::$privacyPolicy->two_policy                = $request->two_policy;
+        self::$privacyPolicy->three_name                = $request->three_name;
+        self::$privacyPolicy->three_policy              = $request->three_policy;
+        self::$privacyPolicy->four_name                 = $request->four_name;
+        self::$privacyPolicy->four_policy               = $request->four_policy;
+        self::$privacyPolicy->save();
     }
 }
